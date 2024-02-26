c293bf16a43456cee29d85df0f44337a4da6adaf
rowdyslav <rowdyslav@gmail.com>
2024-02-26 22:42:22 +0300

    отладка

--- handlers/payment.py
+++ handlers/payment.py
@@ -7,16 +7,15 @@ import config
 
 
 async def pre_checkout_query(pre_checkout_query: PreCheckoutQuery, bot: Bot):
+    ic("pre_checkout_query")
     await bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True)
 
 
 async def success_payment(message: Message):
-    try:
-        with MCRcon(config.RCON_IP, config.RCON_PASSWORD, config.RCON_PORT) as rcon:
-            pay = message.successful_payment
-            assert pay
-            info = pay.order_info
-            assert info
-            rcon.command(f"whitelist add {info.name}")
-    except Exception as e:
-        ic(e)
+    ic("success_payment")
+    with MCRcon(config.RCON_IP, config.RCON_PASSWORD, config.RCON_PORT) as rcon:
+        pay = message.successful_payment
+        assert pay
+        info = pay.order_info
+        assert info
+        rcon.command(f"whitelist add {info.name}")
